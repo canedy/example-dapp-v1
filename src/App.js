192799a4c132a9e4164fbb5e5c3955db1623e977
Bruce <canedy@msn.com>
2022-09-18 22:05:25 -0400

    update 09 18

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+// import { getGreeting } from "./cadence/scripts/getGreeting.js";
+import React, { useEffect, useState } from "react";
+// import { useState } from "react";
+
+const fcl = require("@onflow/fcl");
+const t = require("@onflow/types");
+
+fcl.config()
+    .put("flow.network", "local")
+    .put("accessNode.api", "http://localhost:8080")
+    .put("discovery.wallet", "http://localhost:8701/fcl/authn")
 
 function App() {
+  const [user, setUser] = useState({addr: ''});
+
+  useEffect(() => {
+    fcl.currentUser.subscribe(setUser);
+  }, [])
+
+  const logIn = () => {
+    fcl.authenticate();
+  }
+
+  const logOut = () => {
+    fcl.unauthenticate();
+  }
+
+  // const getTheGreeting = async () => {
+  //   const result = await fcl.send([
+  //     fcl.script(getGreeting)
+  //   ]).then(fcl.decode);
+
+  //   console.log(result);
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user.addr ? user.addr : ""}
+      <button onClick={logIn}>Log In</button>
+      <button onClick={logOut}>Log Out</button>
     </div>
   );
 }
